8a09963984c2d1a54194c2baa6266578395b30a3
Henning Surmeier <h.surmeier@mittwald.de>
2018-09-17 16:59:44 +0200

    fuck

--- target/start_cron.sh
+++ target/start_cron.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-if [ -z "$RESTIC_FILE_LIST" ]; then
+if [ ! -z "$RESTIC_FILE_LIST" ]; then
     sed -i 's/backup \/data/backup %(ENV_RESTIC_FILE_LIST)s/' /etc/supervisor.d/restic.ini
 fi
 echo "${CRON_BACKUP_EXPRESSION} supervisorctl start restic_backup" | crontab -
