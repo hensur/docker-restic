9d014960eefcc8cc1e27ed9bd415811568e55d51
Henning Surmeier <h.surmeier@mittwald.de>
2018-09-17 16:36:53 +0200

    allow file list instead of backup dir

--- target/start_cron.sh
+++ target/start_cron.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
+if [ -z "$RESTIC_FILE_LIST" ]; then
+    sed -i 's/backup \/data/backup %(ENV_RESTIC_FILE_LIST)s/' /etc/supervisor.d/restic.ini
+fi
 echo "${CRON_BACKUP_EXPRESSION} supervisorctl start restic_backup" | crontab -
 crontab -l | { cat; echo "${CRON_CLEANUP_EXPRESSION} supervisorctl start restic_cleanup"; } | crontab -
 /usr/sbin/crond -f
